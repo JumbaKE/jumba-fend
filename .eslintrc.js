4170de92c403af51ddfc83c608058171ccafac9c
hmusonye <humphrey.m.musonye@gmail.com>
2022-01-13 12:12:19 +0300

    (chore): setup project - setup linter - setup airbnb style guide - setup prettier

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["airbnb-base", "prettier"],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 13,
+    sourceType: "module",
+  },
+  plugins: ["prettier"],
+  rules: {
+    "prettier/prettier": "warning",
+  },
+};
